9d43a3f8d2ba72203243c59c091b67af04c3ab10
iAmShivay <shivaysharma77894@gmail.com>
2024-12-08 11:48:59 +0530

    fix: update case description validation to allow minimum length of 1 character

--- src/validators/caseValidator.ts
+++ src/validators/caseValidator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createCaseSchema = z.object({
   type: z.string().min(1).max(100,{message: "Type must be between 5 and 100 characters"}),
   title: z.string().min(1).max(100,{message: "Title must be between 5 and 100 characters"}),
-  description: z.string().min(10,{message: "Description must be at least 10 characters"}),
+  description: z.string().min(1,{message: "Description must be at least 10 characters"}),
   employeeId: z.string(),
   employeeName: z.string().min(1).max(100,{message: "Employee name must be between 5 and 100 characters"}),
   incidentDate: z.string(),
